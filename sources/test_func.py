13a9d8d04d082bec226771b4e7e2915007a1ef15
saurav6129 <saurav.tripathi6129@gmail.com>
2020-08-26 17:31:48 +0530

    resolved

--- sources/test_func.py
+++ sources/test_func.py
@@ -2,15 +2,15 @@ import calculator
 import pytest
  
 def test_addition():
-    assert calulator.addition(10,5)==15
-    assert calulator.addition(10,3)==13
-    assert calulator.addition(10,100)==110
+    assert calculator.addition(10,5)==15
+    assert calculator.addition(10,3)==13
+    assert calculator.addition(10,100)==110
     print("testing build trigger ")
     print("testing again.")
     
 def test_subtraction():
-    assert calulator.subtraction(10,3)==7
-    assert calulator.subtraction(30,10)==20
-    assert calulator.subtraction(25,10)==15
+    assert calculator.subtraction(10,3)==7
+    assert calculator.subtraction(30,10)==20
+    assert calculator.subtraction(25,10)==15
 
 
